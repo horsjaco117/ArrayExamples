96e4ff53797b748bfdd44e42142da72042e99e29
horsjaco117 <jacob_horsley@live.com>
2025-02-10 08:26:53 -0700

    Add simple array example.

--- ArrayExamples/Module1.vb
+++ ArrayExamples/Module1.vb
@@ -1,6 +1,19 @@
-﻿Module Module1
+﻿'header
+
+Option Strict On
+Option Explicit On
+
+Module Module1
 
     Sub Main()
+        'arrays are cool 
+        Dim fruits(5) As String
+
+        fruits(0) = "banana"
+        fruits(3) = "grape"
+        'fruits(6) = "apple" This is out of bounds and won't run
+
+        Console.WriteLine()
 
     End Sub
 
