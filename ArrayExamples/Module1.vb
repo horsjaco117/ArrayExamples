62d725872aab539be0a914e9dd377b949ea5b491
horsjaco117 <jacob_horsley@live.com>
2025-02-10 08:41:19 -0700

    simple iterate on array

--- ArrayExamples/Module1.vb
+++ ArrayExamples/Module1.vb
@@ -8,12 +8,25 @@ Module Module1
     Sub Main()
         'arrays are cool 
         Dim fruits(5) As String
+        Dim names() As String = {"john", "paul", "ringo", "jeff"} 'w/Brackets everything is automatically assigned
+        Dim BeanCounts = New Integer() {12345, 343, 5432, 9876}
+
+
 
         fruits(0) = "banana"
         fruits(3) = "grape"
         'fruits(6) = "apple" This is out of bounds and won't run
 
-        Console.WriteLine()
+        'For i = 0 To 10
+
+        '    Console.WriteLine(names(i))
+
+        'Next 
+        'above comments are examples of breaking 
+
+        For i = LBound(names) To UBound(names)
+            Console.WriteLine(names(i))
+        Next
 
     End Sub
 
